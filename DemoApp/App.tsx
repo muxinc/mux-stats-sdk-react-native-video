a9ff376c64059cf77bc463774f806afd4f03843e
Erik Peña <erikpena@users.noreply.github.com>
2023-01-06 20:06:12 -0800

    Perform version update - Updated react and react-native-video to the latest - Updated DemoApp based on latest version of template from react native - DemoApp is now using Typescript - Added a stub .d.ts for mux-react-native-video-sdk

--- DemoApp/App.tsx
+++ DemoApp/App.tsx
@@ -6,28 +6,25 @@
  * @flow
  */
 
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
-  Text,
   StatusBar,
   Button,
 } from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 import Video from 'react-native-video';
 import muxReactNativeVideo from 'mux-react-native-video-sdk';
 import app from './package.json';
 
 const MuxVideo = muxReactNativeVideo(Video);
 
-const App: () => React$Node = () => {
-  const [showVideo, setShowVideo] = useState(false);
-
-  useEffect(() => {}, []);
+const App: () => React.ReactNode = () => {
+  const [showVideo, setShowVideo] = React.useState(false);
 
   return (
     <>
@@ -36,11 +33,6 @@ const App: () => React$Node = () => {
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
           <View style={styles.body}>
             <Button
               title={showVideo ? 'Hide Player' : 'Show Player'}
---
+++ DemoApp/global.d.ts
@@ -0,0 +1,3 @@
+// The mux-react-native-video-sdk does not currently ship with a .d.ts file,
+// so we stub it out.
+declare module 'mux-react-native-video-sdk';
